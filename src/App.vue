a9dfe97806e13022caf84bb39c824ce40059f9db
CoCo <18290605616@163.com>
2019-08-09 22:56:31 +0800

    更改app.vue配置

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,14 @@
         localStorage.setItem("userId",JSON.stringify(this.$store.state))
       })
       localStorage.getItem("userId") && this.$store.replaceState(Object.assign(this.$store.state,JSON.parse(localStorage.getItem("userId"))));
-    }
+    },
+      mounted(){
+          /**
+           * 解决 css 引入图片在 github pages 无法获取的问题
+           */
+          const { NODE_ENV } = process.env;
+          document.documentElement.className = NODE_ENV;
+      }
   }
 </script>
 
